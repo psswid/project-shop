6725dffdee594612fc30128a00aedf8c080c4ec7
psswid <psswiderski@gmail.com>
2017-11-22 16:52:19 +0100

    Added modal for editing positions

--- admin/admin_list_process.php
+++ admin/admin_list_process.php
@@ -1,4 +1,10 @@
-<?php include '../includes/database.php'?>
+<?php include '../includes/database.php';
+
+  if(isset($_REQUEST['del_item_id'])){
+    $del_sql = "DELETE FROM items WHERE item_id = '$_REQUEST[del_item_id]' ";
+    mysqli_query($conn, $del_sql);
+  }
+?>
 <table class="table table-bordered">
   <thead>
     <tr>
@@ -40,9 +46,93 @@
             <div class='dropdown'>
                 <button class='btn btn-danger dropdown-toggle' data-toggle='dropdown'>Actions <span class='caret'></span></button>
                 <ul class='dropdown-menu dropdown-menu-right'>
-                  <li><a href='#'>Edit</a></li>
-                  <li><a href='#'>Delete</a></li>
-                </ul>
+                  <li>
+                    <a href='#edit_modal' data-toggle='modal'>Edit</a>
+
+                  </li;"; ?>
+                  <li><a href="javascript:;" onclick="del_item(<?php echo $rows['item_id'];?>)">Delete</a></li>
+          <?php  echo "</ul>
+            </div>
+            <div class='modal fade' id='edit_modal'>
+              <div class='modal-dialog'>
+                <div class='modal-content'>
+                <div class='modal-header'>
+                  <button class='close' data-dismiss='modal'>x</button>
+                  <h4>Add new item to base:</h4>
+                  
+                </div>
+                <div class='modal-body'>
+                  <form method='post'>
+                    <div class='form-group'>
+                      <label>Item title</label>
+                      <input type='text' name='item_title' value='$rows[item_title]' class='form-control'>
+                    </div>
+                    <div class='form-group'>
+                      <label>Item description</label>
+                      <input type='text'  name='item_description' value='$rows[item_description]' class='form-control'>
+                    </div>
+                    <div class='form-group'>
+                      <label>Item category</label>
+                      <select class'form-control' name='item_category'>
+                        <option>Select category</option>";
+
+                          $cat_sql = "SELECT * FROM item_cat";
+                          $cat_run = mysqli_query($conn, $cat_sql);
+                          $cat_rows = mysqli_fetch_assoc($cat_run);
+                          if(!$cat_rows){
+                            echo "No result";
+                          }else{
+                            do{
+
+                              $cat_name = ucwords($cat_rows['cat_name']);
+                            if($cat_rows['cat_slug'] ==' '){
+                              $cat_slug=$cat_rows['cat_name'];
+                            }else{
+                              $cat_slug = $cat_rows['cat_slug'];
+                            }
+                            if($cat_slug == $rows['item_cat']){
+                              echo "
+                                <option selected value='$cat_slug'>$cat_rows[cat_name]</option>
+                              ";
+                            }else{
+                              echo "
+                                <option value='$cat_slug'>$cat_rows[cat_name]</option>
+                              ";
+                            }
+                            }while($cat_rows = mysqli_fetch_assoc($cat_run));}
+
+                    echo "</select>
+                    </div>
+                    <div class='form-group'>
+                      <label>Item quantity</label>
+                      <input  type='number' value='$rows[item_qty]'  name='item_qty' class='form-control'>
+                    </div>
+                    <div class='form-group'>
+                      <label>Item cost</label>
+                      <input  type='number' value='$rows[item_cost]'  name='item_cost' class='form-control'>
+                    </div>
+                    <div class='form-group'>
+                      <label>Item price</label>
+                      <input  type='number' value='$rows[item_price]' name='item_price' class='form-control'>
+                    </div>
+                    <div class='form-group'>
+                      <label>Item discount</label>
+                      <input  type='number' value='$rows[item_discount]' name='item_discount' class='form-control'>
+                    </div>
+                    <div class='form-group'>
+                      <label>Item delivery charge</label>
+                      <input  type='number' value='$rows[item_delivery]' name='item_delivery' class='form-control'>
+                    </div>
+                    <div class='form-group'>
+                      <input  type='submit' name='item_submit' class='btn btn-primary btn-block'>
+                    </div>
+                  </form>
+                </div>
+                <div class='modal-footer'>
+                  <button class='btn btn-danger' data-dismiss='modal'>Close</button>
+                </div>
+                </div>
+              </div>
             </div>
           </td>
         </tr>
--- admin/item_list.php
+++ admin/item_list.php
@@ -8,6 +8,7 @@
     $item_price = mysqli_real_escape_string($conn, strip_tags($_POST['item_price']));
     $item_discount = mysqli_real_escape_string($conn, strip_tags($_POST['item_discount']));
     $item_delivery = mysqli_real_escape_string($conn, strip_tags($_POST['item_delivery']));
+
     if(isset($_FILES['item_image']['name'])){
       $file_name = $_FILES['item_image']['name'];
       $path_address = "../images/items/$file_name";
@@ -63,7 +64,17 @@
         xmlhttp.open('GET', 'admin_list_process.php', true);
         xmlhttp.send();
       }
+      function del_item(item_id){
+        xmlhttp.onreadystatechange = function(){
+          if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
 
+            document.getElementById('get_item_list_data').innerHTML = xmlhttp.responseText;
+          }
+        }
+
+        xmlhttp.open('GET', 'admin_list_process.php?del_item_id='+item_id, true);
+        xmlhttp.send();
+      }
 
     </script>
   </head>
@@ -82,19 +93,19 @@
               <form method="post" enctype="multipart/form-data">
                 <div class="form-group">
                   <label for="">Item image</label>
-                  <input name = "item_image" type="file" class = "form-control">
+                  <input  type="file" name = "item_image" class = "form-control">
                 </div>
                 <div class="form-group">
                   <label for="">Item title</label>
-                  <input name = "item_title" type="text" class = "form-control">
+                  <input type="text" name = "item_title" class = "form-control">
                 </div>
                 <div class="form-group">
                   <label for="">Item description</label>
-                  <textarea name = "item_description" class = "form-control"></textarea>
+                  <textarea  name = "item_description" class = "form-control"></textarea>
                 </div>
                 <div class="form-group">
                   <label for="">Item category</label>
-                  <select name="item_category" id="" class="form-control">
+                  <select class="form-control" name="item_category">
                     <option value="">Select category</option>
                     <?php
 
@@ -116,32 +127,30 @@
                           <option value='$cat_slug'>$cat_rows[cat_name]</option>
                         ";}while($cat_rows = mysqli_fetch_assoc($cat_run));}
                     ?>
-
                   </select>
-
                 </div>
                 <div class="form-group">
                   <label for="">Item quantity</label>
-                  <input name = "item_qty" type="number"  class = "form-control">
+                  <input  type="number"  name = "item_qty" class = "form-control">
                 </div>
                 <div class="form-group">
                   <label for="">Item cost</label>
-                  <input name = "item_cost" type="number"  class = "form-control">
+                  <input  type="number"  name = "item_cost" class = "form-control">
                 </div>
                 <div class="form-group">
                   <label for="">Item price</label>
-                  <input name = "item_price" type="number"  class = "form-control">
+                  <input  type="number" name = "item_price" class = "form-control">
                 </div>
                 <div class="form-group">
                   <label for="">Item discount</label>
-                  <input name = "item_discount" type="number"  class = "form-control">
+                  <input  type="number" name = "item_discount" class = "form-control">
                 </div>
                 <div class="form-group">
                   <label for="">Item delivery charge</label>
-                  <input name = "item_delivery" type="number"  class = "form-control">
+                  <input  type="number" name = "item_delivery" class = "form-control">
                 </div>
                 <div class="form-group">
-                  <input name = "item_submit" type="submit"  class = "btn btn-primary btn-block">
+                  <input  type="submit" name = "item_submit" class = "btn btn-primary btn-block">
                 </div>
               </form>
             </div>
